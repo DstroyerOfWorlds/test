efe2b2c90bb19b3617544c0644a7183ae6480f1f
DstroyerOfWorlds <93687396+DstroyerOfWorlds@users.noreply.github.com>
2023-07-28 12:03:02 -0700

    Update navbar-script.js

--- site/navbar-script.js
+++ site/navbar-script.js
@@ -1,9 +1,4 @@
-const nav = document.querySelector('.navbar')
-fetch('/navbar.html')
-.then(res=>res.text())
-.then(data=>{
-    nav.innerHTML=data
-    const parser = new DOMParser()
-    const doc = parser.parseFromString(data, 'text/html')
-    eval(doc.querySelector('script').textContent)
-})
+$(document).ready(function(){
+  $( ".navbar" ).load( "site/navbar.html" );
+  });
+});
